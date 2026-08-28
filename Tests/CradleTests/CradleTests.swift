53a21494d93a3cdf968fdd6084697e074e294f10
opficdev <162981733+opficdev@users.noreply.github.com>
2026-08-29 02:20:18 +0900

    test: Swift Testing으로 대체

--- Tests/CradleTests/CradleTests.swift
+++ Tests/CradleTests/CradleTests.swift
@@ -5,11 +5,10 @@
 //  Created by opfic on 8/28/26.
 //
 
-import XCTest
-@testable import Cradle
+import Testing
+import Cradle
 
-final class CradleTests: XCTestCase {
-	func testCradleModuleIsAvailable() {
-		XCTAssertTrue(true)
-	}
+@Test
+func cradleModuleIsAvailable() {
+	#expect(Bool(true))
 }
