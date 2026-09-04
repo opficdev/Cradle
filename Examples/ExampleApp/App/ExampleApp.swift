adced78cb0063d6323ec40f52569e7374411edf6
opficdev <162981733+opficdev@users.noreply.github.com>
2026-09-04 17:28:00 +0900

    chore: ExampleApp project와 폴더 구조 구성

---
+++ Examples/ExampleApp/App/ExampleApp.swift
@@ -0,0 +1,19 @@
+//
+//  ExampleApp.swift
+//  ExampleApp
+//
+//  Created by opfic on 9/4/26.
+//
+
+import SwiftUI
+
+// Cradle 소비 예제의 앱 진입점
+@main
+struct ExampleApp: App {
+	// ExampleApp 장면 구성
+	var body: some Scene {
+		WindowGroup {
+			EmptyView()
+		}
+	}
+}
